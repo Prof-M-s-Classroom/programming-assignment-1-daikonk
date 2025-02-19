94215758de7e00ad461043f5d7a3f5c7f658ca24
daikonk <nichstoneking@gmail.com>
2025-02-19 23:41:31 +0000

    init

--- main.cpp
+++ main.cpp
@@ -1,36 +1,36 @@
+#include "SpaceRoute.cpp"
 #include <iostream>
 #include <string>
-#include "SpaceRoute.cpp"
 
 using namespace std;
 
 int main() {
-    string mars = "Mars";
-    string jupiter = "Jupiter";
-    string saturn = "Saturn";
-    string earth = "Earth";
-    string venus = "Venus";
+  string mars = "Mars";
+  string jupiter = "Jupiter";
+  string saturn = "Saturn";
+  string earth = "Earth";
+  string venus = "Venus";
 
-    SpaceRoute<string> voyagerRoute;
+  SpaceRoute<string> voyagerRoute;
 
-    voyagerRoute.addWaypointAtEnd(mars);
-    voyagerRoute.addWaypointAtEnd(jupiter);
-    voyagerRoute.addWaypointAtEnd(saturn);
-    voyagerRoute.addWaypointAtBeginning(earth);
-    voyagerRoute.addWaypointAtIndex(2, venus);
+  voyagerRoute.addWaypointAtEnd(mars);
+  voyagerRoute.addWaypointAtEnd(jupiter);
+  voyagerRoute.addWaypointAtEnd(saturn);
+  voyagerRoute.addWaypointAtBeginning(earth);
+  voyagerRoute.addWaypointAtIndex(2, venus);
 
-    cout << "Voyager Route (Forward):\n";
-    voyagerRoute.traverseForward();
+  cout << "Voyager Route (Forward):\n";
+  voyagerRoute.traverseForward();
 
-    cout << "\nVoyager Route (Backward):\n";
-    voyagerRoute.traverseBackward();
+  cout << "\nVoyager Route (Backward):\n";
+  voyagerRoute.traverseBackward();
 
-    cout << "\nPrinting Route: \n";
-    voyagerRoute.print();
+  cout << "\nPrinting Route: \n";
+  voyagerRoute.print();
 
-    voyagerRoute.removeWaypointAtIndex(2);
-    cout << "\nAfter Removing Venus: \n";
-    voyagerRoute.print();
+  voyagerRoute.removeWaypointAtIndex(2);
+  cout << "\nAfter Removing Venus: \n";
+  voyagerRoute.print();
 
-    return 0;
+  return 0;
 }
